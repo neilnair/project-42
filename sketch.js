989448c81db629dfaa84b559b3f25262a1787083
neilnair <77185176+neilnair@users.noreply.github.com>
2021-06-30 20:45:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var hr 
+var mn
+var sc
+
+var hrAngle,mnAngle,scAngle
+function setup() {
+  createCanvas(800,400);
+  angleMode(DEGREES)
+}
+
+function draw() {
+  background(255,255,255);  
+hr = hour();
+mn = minute();
+sc = second();
+scAngle = map(sc,0,60,0,360)
+mnAngle = map(mn,0,60,0,360)
+hrAngle = map(hr%12,0,12,0,360)
+//drawing second hand
+push();
+rotate(scAngle)
+stroke("blue");
+strokeWeight(7);
+line(0,0,100,0)
+pop();
+//drawing minute hand
+push();
+rotate(mnAngle)
+stroke("green")
+strokeWeight(7);
+line(0,0,70,0)
+pop();
+//drawing hour hand
+push();
+rotate(hrAngle)
+stroke("green")
+strokeWeight(7);
+line(0,0,50,0)
+pop();
+//drawing center point
+stroke("black")
+point(0,0)
+//drawing arc
+strokeWeight(10);
+//seconds arc
+stroke("blue")
+arc(0,0,300,300,0,scAngle)
+//minutes arc
+stroke("green")
+arc(0,0,280,280,0,mnAngle)
+//hours arc
+stroke("red")
+arc(0,0,260,260,0,hrAngle)
+
+  drawSprites();
+}
